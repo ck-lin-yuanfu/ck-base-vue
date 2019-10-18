4375ea1d9387cf6de0e0156a0aafa8113c0c236d
ck-lin-yuanfu <2627362847@qq.com>
2019-10-18 16:49:19 +0800

    add file

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+    html {
+        -webkit-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0)
+    }    
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="contains">
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: '欢迎使用 ck-base-vue v 0.0.1'
+    }
+  },
+  components: {},
+  methods: {}
+}
+</script>
+
+<style>
+.contains {
+    width: 960px;
+    margin: 30px auto;
+}
+</style>
